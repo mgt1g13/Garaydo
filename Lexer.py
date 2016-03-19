a9f2c03c48830a9d9c4dbd181d7f4bcee78df36e
Guilherme Gaiardo <guigaiardo@gmail.com>
2016-03-19 14:29:43 -0300

    needs a final revision, but everything seems ok.

--- Lexer.py
+++ Lexer.py
@@ -10,7 +10,8 @@ print("Lexing file -> " + sys.argv[1]);
 
 reserved_words = ["int","float", "char", "do", "for", "while", "if", "else"]
 delimiters = ["(", ")"]
-arithmetic_ops = ["+", "-", "*", "/"]
+arithmetic_ops = ["+", "-", "*", "/", "=", "%"]
+logic_ops = ["<", "<=", ">", ">=", "!=", "=="]
 
 
 
@@ -32,12 +33,45 @@ current_char = next_char()
 
 
 def get_number():
+	#Lê um número composto por digitos
 	current_number = ""
 	global current_char
 	while(current_char.isdigit()):
 		current_number += current_char
 		current_char = next_char()
 	return current_number
+
+def get_word():
+	#Lê uma palavra até encontrar um caractere não alfabético
+	current_word = ""
+	global current_char
+	while(current_char.isalpha() or current_char.isdigit()):
+		current_word += current_char
+		current_char = next_char()
+	return current_word
+
+
+def get_comment():
+	#Le um comentario até encontrar o fechamento do mesmo
+	comment = ""
+	global current_char
+	current_char = next_char()
+	while True:
+		if (current_char == "*"):
+			comment += current_char
+			current_char = next_char()
+			if(current_char == "/"):
+				comment += current_char
+				break
+			else:
+				continue
+		comment += current_char
+		current_char = next_char()
+
+	return comment
+
+
+
 
 
 while(current_char):
@@ -51,11 +85,57 @@ while(current_char):
 		if(current_char == "."):
 			current_char = next_char()
 			current_number += "." + get_number()
-			print("[FLOAT," + current_number+ "]")
+			print("[FLOAT," + current_number + "]")
 		else:
-			print("[INT," + current_number+ "]")
+			print("[INT," + current_number + "]")
 
-		
+	#Palavras e identificadores
+	elif(current_char.isalpha()):
+		current_word = get_word()
+		if (current_word in reserved_words):
+			print("[RESERVED_WORD, " + current_word + "]")
+
+		else:
+			print("[ID, " + current_word + "]")
+
+	#Comentarios
+	elif(current_char == "/"):
+		current_char = next_char()
+		if(current_char == "*"):	#caso seja um comentario
+			comment = "/" + current_char + get_comment()
+			print("[COMMENT, " + comment + "]")
+			current_char = next_char()
+
+		else:	#caso fosse apenas uma /
+			print("[ARITHMETIC_OP, /]")
+			continue
+
+
+
+
+	#Operadores aritmeticos
+	elif(current_char in arithmetic_ops):
+		if(current_char == "/"):
+			current_char = next_char()
+			if(current_char == "/" or current_char == "*"):
+				continue
+		print("[ARITHMETIC_OP, " + current_char + "]")
+		current_char = next_char()
+		continue
+
+
+	#Operadores logicos
+	elif(current_char in logic_ops):
+		print("[LOGIC_OP, " + current_char + "]")
+		current_char = next_char()
+		continue
+
+	#Delimitadores
+	elif(current_char in delimiters):
+		print("DELIMITER, " + current_char + "]")
+		current_char = next_char()
+		continue
+
 
 	else:
 		print("Lexical Error!!!")
